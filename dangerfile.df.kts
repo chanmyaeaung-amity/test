16e4d5ab025178dd453c46655a275e9359fa5a24
chanmyaeaung-amity <chanmyae@amitysolutions.com>
2025-04-19 16:55:49 +0700

    Update dangerfile.df.kts

--- dangerfile.df.kts
+++ dangerfile.df.kts
@@ -3,4 +3,17 @@ import systems.danger.kotlin.*
 danger(args) {
     println("✅ Hello from Danger-Kotlin!")
     warn("This is a test warning from Danger!")
+
+    var allChecksPassed = true
+    val title = github.pr.title
+
+    if (!title.contains(Regex("Feature|Bug|Refactor", RegexOption.IGNORE_CASE))) {
+        warn("PR title should contain 'Feature', 'Bug', or 'Refactor' (case-insensitive).")
+        allChecksPassed = false
+    }
+
+    if (allChecksPassed) {
+        message("✅ All Danger checks passed!")
+    }
+
 }
